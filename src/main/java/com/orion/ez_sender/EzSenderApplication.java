aece96ab361cdcab08ff97ff66030011b360c45c
orion <nnmm5122004@gmail.com>
2026-02-22 23:53:35 +0630

    :tada: init ez sender

---
+++ src/main/java/com/orion/ez_sender/EzSenderApplication.java
@@ -0,0 +1,13 @@
+package com.orion.ez_sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzSenderApplication.class, args);
+	}
+
+}
